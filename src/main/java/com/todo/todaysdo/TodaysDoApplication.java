397f5118c2cf140ead27a906693c2e7aa9b66e51
1seyoung <20171631@edu.hanbat.ac.kr>
2024-07-05 10:21:43 +0900

    init commit

---
+++ src/main/java/com/todo/todaysdo/TodaysDoApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todaysdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodaysDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/todaysdo/TodaysDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todaysdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
